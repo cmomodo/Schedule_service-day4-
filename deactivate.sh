92f1a7049cb5aff60dc13981e6fde7915bd7cb87
cmomodo <ceesay.ml@outlook.com>
2025-01-26 20:08:15 +0000

    This is the ecs container and the deactivating script for the other containers.

---
+++ deactivate.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "This script will delete CloudFormation stacks created."
+echo "Please confirm by entering the stack name(s) you want to deactivate."
+
+# Read input from user
+read -p "Stack Name: "
+
+# Delete the specified stack
+cfn delete-stack --stack-name "$StackName"
